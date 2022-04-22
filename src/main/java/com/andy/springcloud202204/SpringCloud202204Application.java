1783b3509790ce61d59f97a261f52208cf2f9cff
andy_1704680143009850 <andy_170468014300985@ECD.ACS>
2022-04-22 16:54:34 +0800

    first commit

---
+++ src/main/java/com/andy/springcloud202204/SpringCloud202204Application.java
@@ -0,0 +1,13 @@
+package com.andy.springcloud202204;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloud202204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloud202204Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/andy/springcloud202204/SpringCloud202204ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andy.springcloud202204;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloud202204ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
